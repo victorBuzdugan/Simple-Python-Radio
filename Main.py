f85d422713bb028b94356f1a51da57e2478df6dd
buzzvicky <35231162+buzzvicky@users.noreply.github.com>
2023-02-22 16:54:03 +0200

    radio list

--- Main.py
+++ Main.py
@@ -1,8 +1,20 @@
 import vlc
 import time
 
-url = 'https://live.kissfm.ro/kissfm.aacp'
-
+radioStations = (
+    {'name' : 'Kiss FM', 'url' : 'https://live.kissfm.ro/kissfm.aacp'},
+    {'name' : 'Europa FM', 'url' : 'http://astreaming.europafm.ro:8000/europafm_aacp48k'},
+    {'name' : 'Digi FM', 'url' : 'http://edge76.rdsnet.ro:84/digifm/digifm.mp3'},
+    {'name' : 'Radio Iasi?', 'url' : 'http://89.238.227.6:8202/'},
+    {'name' : 'Virgin Radio', 'url' : 'https://astreaming.edi.ro:8443/VirginRadio_aac'},
+    {'name' : 'Radio ZU', 'url' : 'https://ivm.antenaplay.ro/liveaudio/radiozu/playlist.m3u8'},
+    {'name' : 'Magic FM', 'url' : 'https://live.magicfm.ro/magicfm.aacp'},
+    {'name' : 'Rock FM', 'url' : 'https://live.rockfm.ro/rockfm.aacp'},
+    {'name' : 'Pro FM', 'url' : 'https://edge126.rcs-rds.ro/profm/profm.mp3'},
+    {'name' : 'Guerilla', 'url' : 'https://live.guerrillaradio.ro:8443/guerrilla.aac'},
+    {'name' : 'm2O', 'url' : 'https://4c4b867c89244861ac216426883d1ad0.msvdn.net/radiom2o/radiom2o/master_ma.m3u8'},
+    {'name' : 'Antenne Bayern', 'url' : 'https://stream.antenne.de/antenne/stream/mp3'}
+)
 
 #define VLC instance
 instance = vlc.Instance('--input-repeat=-1', '--fullscreen')
@@ -10,15 +22,39 @@ instance = vlc.Instance('--input-repeat=-1', '--fullscreen')
 #Define VLC player
 player = instance.media_player_new()
 
-#Define VLC media
-media=instance.media_new(url)
+for radio in radioStations:
+    radioIndex = radioStations.index(radio)
+    print(radioIndex+1, '-', radioStations[radioIndex]['name'])
 
-#Set player media
-player.set_media(media)
-
-#Play the media
-player.play()
-
-a = input("Asa la misto")
+selectedRadio = 0
+while True:
+    time.sleep(1)
+    selectRadio = input("Select radio number ('0' for exit; default '1'): ") or '1'
+    if selectRadio.lower() == 'n':
+        if selectedRadio == len(radioStations)-1:
+            selectedRadio = 0
+        else:
+            selectedRadio += 1
+    elif selectRadio.lower() == 'p':
+        if selectedRadio == 0:
+            selectedRadio = len(radioStations)-1
+        else:
+            selectedRadio -= 1
+    elif selectRadio == '0':
+        break
+    else:
+        try:
+            selectedRadio = int(selectRadio)-1
+        except:
+            continue
+        else:
+            if selectedRadio < 0 or selectedRadio > len(radioStations)-1:
+                continue
+    media = instance.media_new(radioStations[selectedRadio]['url'])
+    player.set_media(media)
+    player.play()
+    print('\nNow playing:', radioStations[selectedRadio]['name'], '\n')
+    continue
+    
 
 # instance.wait()
