dc844c71306793974c6b58e9fc6c845bb5da27ec
buzzvicky <buzdugan.victor@icloud.com>
2023-02-27 22:06:33 +0200

    json import

--- Main.py
+++ Main.py
@@ -1,25 +1,14 @@
 import vlc
+import json
 from tkinter import *
 from tkinter import ttk
 
-radioStations = {
-    'Kiss FM' : 'https://live.kissfm.ro/kissfm.aacp',
-    'Europa FM' : 'https://astreaming.edi.ro:8443/EuropaFM_aac',
-    'Digi FM' : 'http://edge76.rdsnet.ro:84/digifm/digifm.mp3',
-    'Radio Iasi?' : 'http://89.238.227.6:8202/',
-    'Virgin Radio' : 'https://astreaming.edi.ro:8443/VirginRadio_aac',
-    'Radio ZU' : 'https://ivm.antenaplay.ro/liveaudio/radiozu/playlist.m3u8',
-    'Magic FM' : 'https://live.magicfm.ro/magicfm.aacp',
-    'Rock FM' : 'https://live.rockfm.ro/rockfm.aacp',
-    'Pro FM' : 'https://edge126.rcs-rds.ro/profm/profm.mp3',
-    'Guerilla' : 'https://live.guerrillaradio.ro:8443/guerrilla.aac',
-    'm2O' : 'https://4c4b867c89244861ac216426883d1ad0.msvdn.net/radiom2o/radiom2o/master_ma.m3u8',
-    'Antenne Bayern' : 'https://stream.antenne.de/antenne/stream/mp3'
-}
-
+#import radio stations file
+with open('RadioStations.json', 'r') as jsonFile:
+  radioStations = json.load(jsonFile)
+radioStations.pop("_comment")
+DEFAULT_RADIO = list(radioStations.keys())[0]
 radioStationsNames = sorted(radioStations, key=str.lower)
-
-DEFAULT_RADIO = 'Kiss FM'
 
 def playRadio():
     radio = selectedRadio.get()
